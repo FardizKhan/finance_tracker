0acf58eed7fd2b166ec425f5d83d1c96cec9541c
fardiz <126541540+fardiz@users.noreply.github.com>
2026-09-16 15:41:17 +1000

    Creating the text form fields where details of the expense is added.

--- lib/screens/add_expense.dart
+++ lib/screens/add_expense.dart
@@ -1,12 +1,57 @@
 import 'package:flutter/material.dart';
 
-class AddExpense extends StatelessWidget {
+
+
+class AddExpense extends StatefulWidget {
   const AddExpense({super.key});
 
+  @override
+  State<AddExpense> createState() => _AddExpenseState();
+}
+
+class _AddExpenseState extends State<AddExpense> {
+  final _formKey = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text("Add Expense"),),
+      body: Padding(
+        padding: const EdgeInsets.all(16),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            children: [
+              TextFormField(
+                decoration: const InputDecoration(
+                  labelText: 'Expense',
+                  border: OutlineInputBorder(),
+                  
+                ),
+                validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return 'Please enter what the expense was.';
+                }
+                return null; // Null means there is no error
+              },
+              ),
+              Padding(padding: const EdgeInsets.all(5)),
+              TextFormField(
+                decoration: const InputDecoration(
+                  labelText: 'Category',
+                  border: OutlineInputBorder(),
+                  
+                ),
+                validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return 'Please enter the category.';
+                }
+                return null; // Null means there is no error
+              },
+              )
+            ],
+          )
+        ), 
+      ),
     );
   }
 }
